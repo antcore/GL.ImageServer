18bb3fbe0f0ce76d7387bb7a37a7de7bbbc2b4b0
wwq <contactmewwq@163.com>
2016-06-02 10:44:24 +0800

    维护一下

--- GL.ImageServer/GL.ApiServer/Controllers/UpController.cs
+++ GL.ImageServer/GL.ApiServer/Controllers/UpController.cs
@@ -18,6 +18,14 @@ namespace GL.ApiServer.Controllers
     /// </summary>
     public class UpController : ApiController
     {
+        
+        public class UpdataModel
+        {
+            public string appid { get; set; }
+            public string dirid { get; set; }
+        }
+
+        #region web 表单[multipart/form-data]提交文件
         /// <summary>
         /// 文件上传处理接口 -- 接收表单[multipart/form-data]提交文件 处理接口
         /// </summary>
@@ -28,130 +36,128 @@ namespace GL.ApiServer.Controllers
         public async Task<HttpResponseMessage> Post(string appId, string dirId)
         {
             HelperResultMsg result = new HelperResultMsg();
-            // multipart/form-data
-            var provider = new MultipartMemoryStreamProvider();
-            await Request.Content.ReadAsMultipartAsync(provider);
-            //文件信息
-            GL_Images img = null;
-            List<GL_Images> listFiles = new List<GL_Images>();
-            //返回客户端信息
-            List<string> resultString = new List<string>();
-            string filenames;
-            Stream ms;
-            byte[] data;
-            FileInfo info = null;
-            try
+            if (string.IsNullOrEmpty(appId) || appId.Length != 32)
             {
-                foreach (var item in provider.Contents)
+                result.message = "不合法的 appid";
+
+            }
+            else
+            {
+                // multipart/form-data
+                var provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+                //文件信息
+                GL_Images img = null;
+                List<GL_Images> listFiles = new List<GL_Images>();
+                //返回客户端信息
+                List<string> resultString = new List<string>();
+                string filenames;
+                Stream ms;
+                byte[] data;
+                FileInfo info = null;
+                try
                 {
-                    if (item.Headers.ContentDisposition.FileName != null)
+                    foreach (var item in provider.Contents)
                     {
-                        filenames = (item.Headers.ContentDisposition.FileName).Replace("\"", "");
-                        //Strem
-                        ms = item.ReadAsStreamAsync().Result;
-
-                        using (var br = new BinaryReader(ms))
+                        if (item.Headers.ContentDisposition.FileName != null)
                         {
-                            if (ms.Length <= 0)
-                                break;
-                            data = br.ReadBytes((int)ms.Length);
+                            filenames = (item.Headers.ContentDisposition.FileName).Replace("\"", "");
+                            //Strem
+                            ms = item.ReadAsStreamAsync().Result;
 
-                            //FileInfo
-                            info = new FileInfo(item.Headers.ContentDisposition.FileName.Replace("\"", ""));
-
-                            //Create
-                            img = new GL_Images
+                            using (var br = new BinaryReader(ms))
                             {
-                                sId = HelperAutoGuid.Instance.GetGuidString(),
-                                sFileName = info.Name.Substring(0, info.Name.LastIndexOf(".")),
-                                sFileSiffix = info.Extension.Substring(1).ToLower(),
-                                iFileSize = ms.Length, //文件大小
-                                iSource = 1, //文件来源 web
-                                //
-                                sDirId = dirId,
-                                //sUriDomain = ServerSetting.sServerUriDomain,
-                                //sUriPath = string.Empty,
-                                //sFilePath = string.Empty,
-                                dCreateTime = DateTime.Now
-                            };
-                            // 盘符或者服务网站根目录 + 存储文件夹 + 年月动态文件夹
-                            img.sFilePath = string.Format(@"{0}/{1}/{2}", ServerSetting.SaveDisc, ServerSetting.ServerPath, img.dCreateTime.ToString("yyyyMM"));
-                            //Write File
+                                if (ms.Length <= 0)
+                                    break;
+                                data = br.ReadBytes((int)ms.Length);
 
-                            //判断文件路径
-                            if (!Directory.Exists(img.sFilePath))
-                            {
-                                Directory.CreateDirectory(img.sFilePath);
+                                //FileInfo
+                                info = new FileInfo(item.Headers.ContentDisposition.FileName.Replace("\"", ""));
+
+                                //Create
+                                img = new GL_Images
+                                {
+                                    sId = HelperAutoGuid.Instance.GetGuidString(),
+                                    sFileName = info.Name.Substring(0, info.Name.LastIndexOf(".")),
+                                    sFileSiffix = info.Extension.Substring(1).ToLower(),
+                                    iFileSize = ms.Length, //文件大小
+                                    iSource = 1, //文件来源 web
+                                                 //
+                                    sDirId = dirId, sAppId = appId,
+                                    //sUriDomain = ServerSetting.sServerUriDomain,
+                                    //sUriPath = string.Empty,
+                                    //sFilePath = string.Empty,
+                                    dCreateTime = DateTime.Now
+                                };
+                                // 盘符或者服务网站根目录 + 存储文件夹 + 年月动态文件夹
+                                img.sFilePath = string.Format(@"{0}/{1}/{2}", ServerSetting.SaveDisc, ServerSetting.ServerPath, img.dCreateTime.ToString("yyyyMM"));
+                                //Write File
+
+                                //判断文件路径
+                                if (!Directory.Exists(img.sFilePath))
+                                {
+                                    Directory.CreateDirectory(img.sFilePath);
+                                }
+                                //文件存储文件路径
+                                img.sFilePath = string.Format(@"{0}/{1}.{2}", img.sFilePath, img.sId, img.sFileSiffix);
+                                //写入磁盘
+                                File.WriteAllBytes(img.sFilePath, data);
+
+                                //获取文件路径--备用
+                                img.sUriDomain = ServerSetting.sServerUriDomain;
+                                img.sUriPath = string.Format(@"/api/down/{0}.{1}", img.sId, img.sFileSiffix);
+
+                                listFiles.Add(img);
+
+                                //获取文件请求路径
+                                resultString.Add(string.Format(@"{0}{1}", img.sUriDomain, img.sUriPath));
                             }
-                            //文件存储文件路径
-                            img.sFilePath = string.Format(@"{0}/{1}.{2}", img.sFilePath, img.sId, img.sFileSiffix);
-                            //写入磁盘
-                            File.WriteAllBytes(img.sFilePath, data);
-
-                            //获取文件路径--备用
-                            img.sUriDomain = ServerSetting.sServerUriDomain;
-                            img.sUriPath = string.Format(@"/api/down/{0}.{1}", img.sId, img.sFileSiffix);
-
-                            listFiles.Add(img);
-
-                            //获取文件请求路径
-                            resultString.Add(string.Format(@"{0}{1}", img.sUriDomain, img.sUriPath));
+                        }
+                    }
+                    if (listFiles.Count > 0)
+                    {
+                        //保存至数据库
+                        var msg = new ImageService().InsertAll(listFiles);
+                        if (msg.success)
+                        {
+                            result.success = true;
+                            result.message = "上传成功";
+                            result.resultInfo = resultString;
                         }
                     }
                 }
-                if (listFiles.Count > 0)
+                catch (Exception ex)
                 {
-                    //保存至数据库
-                    var msg = new ImageService().InsertAll(listFiles);
-                    if (msg.success)
-                    {
-                        result.success = true;
-                        result.message = "上传成功";
-                        result.resultInfo = resultString;
-                    }
+                    HelperNLog.Default.Error("[图片服务器]表单[multipart/form-data]-上传异常", ex);
+                    result.message = "表单[multipart/form-data]-上传出现异常";
                 }
-            }
-            catch (Exception ex)
-            {
-                HelperNLog.Default.Error("[图片服务器]表单[multipart/form-data]-上传异常", ex);
-                result.message = "表单[multipart/form-data]-上传出现异常";
             }
             return new HttpResponseMessage
             {
                 Content = new StringContent(JsonConvert.SerializeObject(result), System.Text.Encoding.UTF8, "application/json")
             };
         }
+        #endregion
+         
+        #region 微信 传入 token与mediaid 下载图片
 
-
-
-
-
-        public class DownWeChatImage
-        {
-            public string imageserver_appid { get; set; }
-            public string dirId { get; set; }
+        public class UpWeChatUpdataModel: UpdataModel
+        { 
             public string media_ids { get; set; }
             public string app_token { get; set; }
-        }
-        public byte[] StreamToBytes(Stream stream)
-        {
-            byte[] bytes = new byte[stream.Length];
-            stream.Read(bytes, 0, bytes.Length);
-            // 设置当前流的位置为流的开始
-            stream.Seek(0, SeekOrigin.Begin);
-            return bytes;
         }
         // POST: api/WeChat 
         /// <summary>
         /// 下载微信服务器上图片
         /// </summary>
         /// <returns></returns> 
-        public HttpResponseMessage WeChat([FromBody]DownWeChatImage param)
+        public HttpResponseMessage WeChat([FromBody]UpWeChatUpdataModel param)
         {
             HelperResultMsg result = new HelperResultMsg();
             string media_ids = param.media_ids;
-            if (string.IsNullOrEmpty(param.imageserver_appid)|| param.imageserver_appid.Length!=32) {
-                result.message = "不合法的 imageserver_appid";
+            if (string.IsNullOrEmpty(param.appid) || param.appid.Length != 32)
+            {
+                result.message = "不合法的 appid";
             }
             else if (media_ids.Length <= 0)
             {
@@ -192,8 +198,8 @@ namespace GL.ApiServer.Controllers
                                 sFileSiffix = "jpg",
                                 iFileSize = ms.Length, //文件大小
                                 iSource = 2, //文件来源 wechat 
-                                sDirId = param.dirId,
-                                //param.imageAppId
+                                sDirId = param.dirid,
+                                sAppId= param.appid, 
                                 dCreateTime = DateTime.Now
                             };
                             // 盘符或者服务网站根目录 + 存储文件夹 + 年月动态文件夹
@@ -222,12 +228,18 @@ namespace GL.ApiServer.Controllers
                     HelperNLog.Default.Error("[图片服务器]wechat-上传异常", ex);
                     result.message = "wechat-上传出现异常";
                 }
-            } 
+            }
             return new HttpResponseMessage
             {
                 Content = new StringContent(JsonConvert.SerializeObject(result), System.Text.Encoding.UTF8, "application/json")
             };
         }
+        #endregion
+
+
+
+
+
 
     }
 }
---
+++ GL.ImageServer/GL.DBOptions/Migrations/201606020115218_update_imgage_appid.cs
@@ -0,0 +1,20 @@
+namespace GL.DBOptions.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update_imgage_appid : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.GL_Images", "sAppId", c => c.String(nullable: false, maxLength: 32));
+            AlterColumn("dbo.GL_Images", "sDirId", c => c.String(maxLength: 32, fixedLength: true, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.GL_Images", "sDirId", c => c.String(nullable: false, maxLength: 32, fixedLength: true, unicode: false));
+            DropColumn("dbo.GL_Images", "sAppId");
+        }
+    }
+}
